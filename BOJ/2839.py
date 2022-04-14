1b932ebaffd331fd7651a7c953f39710db237297
Sukyeong Seo <ssk0967@naver.com>
2022-04-14 16:59:03 +0900

    Create 2839.py

---
+++ BOJ/2839.py
@@ -0,0 +1,14 @@
+# 설탕 배달 - 그리디 알고리즘
+
+sugar = int(input())
+bag = 0
+
+while sugar >= 0 :
+    if sugar % 5 == 0 :  
+        bag += (sugar // 5) 
+        print(bag)
+        break
+    sugar -= 3  
+    bag += 1 
+else :
+    print(-1)
