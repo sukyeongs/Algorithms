94843342c5c5f72b581459af215f1301d6cefa2f
Sukyeong Seo <ssk0967@naver.com>
2022-04-16 00:24:44 +0900

    Create 1316.py

---
+++ BOJ/Implementation/1316.py
@@ -0,0 +1,13 @@
+# 그룹 단어 체커 - Implementation
+
+N = int(input())
+result = N
+for i in range(0,N):
+    word=input()
+    for j in range(0,len(word)-1):
+        if word[j]==word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            result-=1
+            break
+print(result)
