8cdb80770256a3a4f3dd3dddb7b540258f302cb9
sukyeong <ssk0967@naver.com>
2022-05-04 01:32:40 +0900

    리스트를 문자열로 합치기

---
+++ WIL/리스트를_문자열로_합치기.py
@@ -0,0 +1,7 @@
+# 리스트 -> 문자열 출력
+str_list = ['h', 'e', 'l', 'l', 'o']
+result = "".join(str_list)  # result = "hello"
+
+result2 = " ".join(str_list)  # result2 = "h e l l o"
+
+result3 = ".".join(str_list)  # result3 = "h.e.l.l.o"
