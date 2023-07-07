1387512c3b963bcd571201369882c7ebefc0dde8
Sukyeong Seo <ssk0967@naver.com>
2023-07-07 09:36:56 +0900

    1012 - 유기농 배추
    유기농배추 복습

--- BOJ/BFS/1012.py
+++ BOJ/BFS/1012.py
@@ -1,13 +1,15 @@
 # 1012 - 유기농 배추
 
 import sys
-sys.setrecursionlimit(10000)  # 재귀함수 한도 제한 해제
+
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-T = int(input().rstrip())  # 테스트케이스 개수
+T = int(input().rstrip())
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
 
 def BFS(x, y):
     queue = [(x, y)]
@@ -15,30 +17,31 @@ def BFS(x, y):
 
     while queue:
         x, y = queue.pop()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= M or ny > N or ny < 0:
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
                 continue
 
             if matrix[nx][ny] == 1:
                 queue.append((nx, ny))
                 matrix[nx][ny] = 0
 
-for i in range(T):
-    M, N, K = list(map(int, input().split()))  # 가로, 세로, 배추 개수
+
+for t in range(T):
+    M, N, K = map(int, input().split())
     matrix = [[0] * N for _ in range(M)]
     cnt = 0
 
-    for j in range(K):
+    for i in range(K):
         x, y = map(int, input().split())
         matrix[x][y] = 1
 
-    for a in range(M):
-        for b in range(N):
-            if matrix[a][b] == 1:
-                BFS(a, b)
+    for m in range(M):
+        for n in range(N):
+            if matrix[m][n] == 1:
+                BFS(m, n)
                 cnt += 1
+
     print(cnt)
