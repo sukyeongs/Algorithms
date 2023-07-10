1118a4517935047afa4ef3bb24e6177c9a31677f
Sukyeong Seo <ssk0967@naver.com>
2023-07-10 09:29:13 +0900

    1927 - 최소 힙

---
+++ BOJ/PriorityQueue/1927.py
@@ -0,0 +1,19 @@
+# 1927 - 최소 힙
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print("0")
+
+    else:
+        heapq.heappush(heap, x)
