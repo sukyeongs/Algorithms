2e9ce096668b2a1a0459bfb832a98beac54ef1b7
Sukyeong Seo <ssk0967@naver.com>
2024-03-20 14:14:08 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,19 +1,22 @@
-def time_to_day(date):
-    y, m, d = map(int, date.split('.'))
-    return y * 12 * 28 + m * 28 + d
-
+def date_to_day(date, m):
+    year, month, day = map(int, date.split("."))
+    month += m
+    return year * 12 * 28 + month * 28 + day
 
 def solution(today, terms, privacies):
     answer = []
-    terms_period = {}
     
+    term_dict = {}
     for term in terms:
-        term_type, period = term.split()
-        terms_period[term_type] = int(period) * 28
-        
-    for index, privacy in enumerate(privacies):
-        date, term = privacy.split()
-        if time_to_day(date) + terms_period[term] <= time_to_day(today):
-            answer.append(index + 1)
+        term_type, term_period = term.split()
+        term_dict[term_type] = int(term_period)
 
+    today_day = date_to_day(today, 0)
+    for index, privacy in enumerate(privacies):
+        privacy_date, term = privacy.split()
+        expired_day = date_to_day(privacy_date, term_dict[term])
+        
+        if expired_day <= today_day:
+            answer.append(index + 1)
+            
     return answer
