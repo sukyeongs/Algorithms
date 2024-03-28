c873474f02891ed91f6b94e1d5361356c031167f
Sukyeong Seo <ssk0967@naver.com>
2024-03-28 11:09:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,2 @@
 def solution(nums):
-    n = len(nums)  # 포켓몬 수
-    pocketmon = set(nums)
-    
-    if len(pocketmon) >= n//2:
-        return n//2
-    
-    if n//2 == 1:
-        return 1
-    
-    return len(pocketmon)
+    return min(len(nums)/2, len(set(nums)))
