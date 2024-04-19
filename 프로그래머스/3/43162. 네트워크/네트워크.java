eed2c21af6d1fccd5ce91f98076e542023d1b643
Sukyeong Seo <ssk0967@naver.com>
2024-04-19 15:03:55 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,38 +1,40 @@
 import java.util.*;
 
 class Solution {
-    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    ArrayList<Integer>[] graph;
     boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        graph = new ArrayList[n];
         visited = new boolean[n];
         
         for(int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            graph[i] = new ArrayList<>();
         }
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(i != j && computers[i][j] == 1) {
-                    graph.get(i).add(j);
+                if(computers[i][j] == 1 && i != j) {
+                    graph[i].add(j);
                 }
             }
         }
         
         for(int i = 0; i < n; i++) {
-            if(visited[i] == false) {
+            if(!visited[i]) {
                 answer++;
                 dfs(i);
             }
         }
-
+        
         return answer;
     }
     
     void dfs(int cur) {
         visited[cur] = true;
-        for(Integer node: graph.get(cur)) {
-            if(visited[node] == false) {
+        for(Integer node: graph[cur]) {
+            if(!visited[node]) {
+                visited[node] = true;
                 dfs(node);
             }
         }
