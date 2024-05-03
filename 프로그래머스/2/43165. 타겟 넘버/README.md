e23d22f7680fe0e2ad65f6ddf14617dd107ae0f4
Sukyeong Seo <ssk0967@naver.com>
2024-05-03 10:51:17 +0900

    [level 2] Title: 타겟 넘버, Time: 2.70 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 5.26 ms
+메모리: 10.4 MB, 시간: 2.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 19일 14:39:08
+2024년 05월 03일 10:51:15
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,15 +2,20 @@ answer = 0
 
 def dfs(numbers, target, cur, depth):
     global answer
-    if len(numbers) == depth:
+    
+    if depth == len(numbers):
         if cur == target:
             answer += 1
-    else:
-        dfs(numbers, target, cur + numbers[depth], depth + 1)
-        dfs(numbers, target, cur - numbers[depth], depth + 1)
+        return
+    
+    dfs(numbers, target, cur + numbers[depth], depth + 1)
+    dfs(numbers, target, cur - numbers[depth], depth + 1)
+    
 
-        
 def solution(numbers, target):
     global answer
-    dfs(numbers, target, 0, 0)
+    
+    dfs(numbers, target, numbers[0], 1)
+    dfs(numbers, target, -numbers[0], 1)
+    
     return answer
