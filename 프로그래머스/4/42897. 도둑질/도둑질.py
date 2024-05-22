edbdad57f6ef5915f6014b362f46e93dd9d29566
Sukyeong Seo <ssk0967@naver.com>
2024-05-23 01:17:30 +0900

    [level 4] Title: 도둑질, Time: 545.28 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -6,7 +6,7 @@ def solution(money):
     for i in range(1, len(money) - 1):
         dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i])
     
-    # dp1[0] = 0
+    dp2[0] = 0
     for i in range(1, len(money)):
         dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i])
 
