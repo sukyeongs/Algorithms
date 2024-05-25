456fd6391eaf0a2835351a08bb49d8e8efaa62c3
Sukyeong Seo <ssk0967@naver.com>
2024-05-25 17:31:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.42 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,24 @@
 from collections import deque
 
 def solution(maps):
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-    row_cnt, col_cnt = len(maps), len(maps[0])
-
-    visited = [[0] * col_cnt for _ in range(row_cnt)]
+    n = len(maps)
+    m = len(maps[0])
+    visited = [[0] * m for _ in range(n)]
+    
     queue = deque()
     queue.append([0, 0])
+    visited[0][0] = 1
     
+    dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if (0 <= nx < row_cnt and 0 <= ny < col_cnt and maps[nx][ny] != 0):
-                if (visited[nx][ny] == 0):
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.append([nx, ny])
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and maps[nx][ny] == 1:
+                queue.append([nx, ny])
+                visited[nx][ny] = visited[x][y] + 1
     
-    if visited[row_cnt-1][col_cnt-1] == 0:
-        answer = -1
-    else:
-        answer = visited[row_cnt-1][col_cnt-1] + 1
-        
-    return answer
+    if visited[n-1][m-1] != 0:
+        return visited[n-1][m-1]
+    
+    return -1
