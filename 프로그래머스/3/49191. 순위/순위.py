fcddc5e385fa8391181fb1504b170838c7bc77d9
Sukyeong Seo <ssk0967@naver.com>
2024-05-30 22:52:52 +0900

    [level 3] Title: 순위, Time: 87.28 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,21 +1,21 @@
 def solution(n, results):
     answer = 0
-    record = {i + 1: [set(), set()] for i in range(n)}
-
-    for result in results:
-        record[result[0]][0].add(result[1])
-        record[result[1]][1].add(result[0])
-
-    for _ in range(2):
-        for i in record.keys():
-            for wins in record[i][0]:
-                record[i][0] = record[i][0].union(record[wins][0])
-
-            for loses in record[i][1]:
-                record[i][1] = record[i][1].union(record[loses][1])
-
-    for i in record.keys():
-        if len(record[i][0]) + len(record[i][1]) == n - 1:
+    board = [[0]*n for _ in range(n)]
+    
+    for winner, loser in results:
+        board[winner-1][loser-1] = 1
+        board[loser-1][winner-1] = -1
+    
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if i == j or board[i][j] in [1,-1]:
+                    continue
+                if board[i][k] == board[k][j] == 1:
+                    board[i][j] = 1
+                    board[j][i] = board[k][i] = board[j][k] = -1
+    
+    for row in board:
+        if row.count(0) == 1:
             answer += 1
-
     return answer
